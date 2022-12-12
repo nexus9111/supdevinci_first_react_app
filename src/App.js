10b93bc07460a3730d360bdca1acc6810592a516
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2022-12-12 15:09:22 +0100

    :tada: add key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Articles from "./Articles";
+import RandomCocktail from "./RandomCocktail";
 
 function App() {
   return (
     <div>
       <Header />
       <Articles />
+      <RandomCocktail />
     </div>
   );
 }
