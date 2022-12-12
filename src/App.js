cf0105e09e686fbb0b9a4da3cbe792eaa10e5029
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2022-12-12 15:57:10 +0100

    :tada: work on states

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Header from "./Header";
 import Articles from "./Articles";
+import Divider from "./Divider";
 import RandomCocktail from "./RandomCocktail";
+
 
 function App() {
   return (
     <div>
       <Header />
+      <Divider title="Articles" />
       <Articles />
+      <Divider title="Random cocktail" />
       <RandomCocktail />
     </div>
   );
