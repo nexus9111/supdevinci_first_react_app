e08bac650c1ebd5a45c4d49125776d1f3ef690ae
J.Nexus <46869823+nexus9111@users.noreply.github.com>
2022-12-13 10:06:26 +0100

    ♻️  lil refacto

--- src/CocktailsController/LoadingCocktailCard.js
+++ src/CocktailsController/LoadingCocktailCard.js
@@ -3,9 +3,6 @@ import { Card } from "antd";
 const LoadingCocktailCard = (props) => {
     return (
         <Card style={{ width: 300, marginTop: 16 }} loading={true}>
-            <p>Card content</p>
-            <p>Card content</p>
-            <p>Card content</p>
         </Card>
     );
 };
